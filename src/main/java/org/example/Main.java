e81d95bc77526abbae45a1a30e32664da0be4c96
SaszaTkachuk <alt.wizio@gmail.comm>
2024-01-30 16:59:33 +0100

    first init  commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+
+    /**
+     * @Document (пояснення)
+     * Java Doc (якась документація. Зазвичай пишеться над методами або над класами)
+     */
+    public static void main(String[] args) {
+//        This is first string (line)
+        System.out.println("FIRST STRING (line)");
+//        This is second string (line)
+        System.out.println("SECOND STRING (line)");
+
+        System.out.println("Sum : " + sum(2222022, 78));
+        /*
+        System.out.println("SECOND STRING (line)");
+        System.out.println("SECOND STRING (line)");
+        System.out.println("SECOND STRING (line)");
+         */
+//        TODO: Fix this (Наприклад, щось, що обов'язково треба буде колись виправити)
+
+    }
+
+    public static int sum(int first, int second) {
+        return first + second;
+    }
+}
