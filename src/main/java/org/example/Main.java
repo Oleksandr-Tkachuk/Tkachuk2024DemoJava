71297db6cfb0b644879c6e50f603dfcb1b933a37
SaszaTkachuk <alt.wizio@gmail.comm>
2024-01-30 21:43:44 +0100

    Feature is ready (Другий коментар, але це той самий тільки вже через готову панель в Intelij IDEA. Зміни теж були вислані через цю саму панель.)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     /**
@@ -20,6 +22,17 @@ public class Main {
          */
 //        TODO: Fix this (Наприклад, щось, що обов'язково треба буде колись виправити)
 
+
+/*
+        git flow - це набір Git-розширень, які надають вам більше зручний та вищий рівень абстракції над використанням гілок у Git.
+        Це є методологія ведення гілок у Git, яка спрощує процес розробки та випуску програмного забезпечення.
+
+        git flow додає нові команди до Git, які полегшують створення та управління цими гілками.
+        Використання git flow може бути особливо корисним для команд, які ведуть регулярний випуск програмного забезпечення та хочуть забезпечити стабільність та організованість у розробці.
+ */
+
+//        LATER CREATED NEW BRANCH (---> git checkout -b "feature_branch" --->)
+        System.out.println("New feature development...");
     }
 
     public static int sum(int first, int second) {
